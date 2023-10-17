527b2f4f2c5af722b4c388fa74e084b0e960d786
vartika4160 <139868043+vartika4160@users.noreply.github.com>
2023-10-17 20:31:19 +0530

    Create hello_world.cpp

---
+++ hello_world.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i;
+    for (i = 0; i < 10; i++)
+    {
+        cout << i +1 << " Hello World" << endl;
+    }
+    
+    return 0;
+}
