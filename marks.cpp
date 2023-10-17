47a4911207be4697eb3101fc47bc43621e1ca188
vartika4160 <139868043+vartika4160@users.noreply.github.com>
2023-10-17 21:06:28 +0530

    Create marks.cpp

---
+++ marks.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int marks[5];
+    cout<<"enter the marks"<<endl;
+    for(int i=0; i<5; i++)
+    {
+        cin>>marks[i];
+    }
+    for(int i=0; i<5; i++)
+    { cout<<marks[i]<<endl;
+    }
+}
